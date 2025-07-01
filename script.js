ce20970b53c94fcc862fd356b317933d2caeea5e
bibi11092006 <138732150+bibi11092006@users.noreply.github.com>
2025-07-01 20:28:49 -0300

    aula mmmmmmmmm

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function diminuiTamanho(){
     //tamanhoSenha = tamanhoSenha-1;
     if (tamanhoSenha > 1){
         tamanhoSenha--;
-        numeroSenha.textContent = tamanhoSenha;
     }
+    numeroSenha.textContent = tamanhoSenha
 }
 
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
         tamanhoSenha++;
-        numeroSenha.textContent = tamanhoSenha;
     }
+    numeroSenha.textContent = tamanhoSenha
 }
